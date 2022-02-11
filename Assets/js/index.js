94fe9ce7951fe48f4a2686ff7b2dcb2d09edce80
Mohsin <mohsinnali714@gmail.com>
2022-02-11 17:00:36 +0000

    ok

---
+++ Assets/js/index.js
@@ -0,0 +1,14 @@
+
+// window.onload=my_code();
+// function my_code(){
+//     const togglePassword = document.querySelector('#togglepswd');
+//     const password = document.querySelector('#pswd-input');
+   
+//     togglePassword.addEventListener('click', function (e) {
+//       // toggle the type attribute
+//       const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+//       password.setAttribute('type', type);
+//       // toggle the eye slash icon
+//       this.classList.toggle('fa-eye-slash');
+//   });
+// }
